174c02a4d37398da15dad97ae99d03a24a5c48f9
Brian T <125165526+JBBrian@users.noreply.github.com>
2023-09-05 13:27:34 -0700

    Add files via upload
    still developing more queries but will add file for backup

---
+++ Adventure Works Queries.sql
@@ -0,0 +1,40 @@
+-- All employees pay rate
+SELECT 
+	p.FirstName,
+	p.LastName,
+	hr.JobTitle,
+	ep.Rate
+FROM Person.Person p
+LEFT JOIN HumanResources.Employee hr
+ON p.BusinessEntityID = hr.BusinessEntityID
+LEFT JOIN HumanResources.EmployeePayHistory ep
+ON p.BusinessEntityID = ep.BusinessEntityID
+ORDER BY ep.Rate DESC;
+
+-- Sales person w/ bonus of 3000 or above
+SELECT p.FirstName, p.LastName, s.Bonus
+FROM Person.Person p
+LEFT JOIN Sales.SalesPerson s
+ON s.BusinessEntityID = p.BusinessEntityID
+WHERE s.Bonus >= 3000
+ORDER BY s.Bonus DESC;
+
+-- SELECT EMPLOYEE WHO DONT HAVE ORDERS WITH 'Riders Company
+SELECT FirstName
+FROM Person.Person
+WHERE FirstName NOT IN (SELECT p.FirstName
+					FROM Person.Person p
+					JOIN Sales.Store v
+					ON p.BusinessEntityID = v.SalesPersonID
+					WHERE v.name = 'Riders Company')
+;
+
+-- SHOW ALL SALES PEOPLE AND THEIR ORDER COUNTS
+SELECT p.FirstName,
+	   COUNT(v.name) AS OrderCount
+FROM Person.Person p
+LEFT JOIN Sales.Store v
+ON p.BusinessEntityID = v.SalesPersonID
+WHERE p.PersonType = 'SP'
+GROUP BY p.FirstName
+ORDER BY OrderCount DESC
